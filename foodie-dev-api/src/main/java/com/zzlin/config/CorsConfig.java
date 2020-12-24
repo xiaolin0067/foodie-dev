0826e572f37c1cb3343da4c1490e12210334808d
zlin <1562627895@qq.com>
2020-12-24 22:00:38 +0800

    跨域问题

---
+++ foodie-dev-api/src/main/java/com/zzlin/config/CorsConfig.java
@@ -0,0 +1,34 @@
+package com.zzlin.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @author zlin
+ * @date 20201224
+ */
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        // 1. 添加cors配置信息
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://localhost:8080");
+        // 设置允许请求的方式
+        configuration.addAllowedMethod("*");
+        // 设置允许的header
+        configuration.addAllowedHeader("*");
+        // 是否发送cookie信息
+        configuration.setAllowCredentials(true);
+
+        // 2. 为url添加映射路径
+        UrlBasedCorsConfigurationSource corsSource = new UrlBasedCorsConfigurationSource();
+        corsSource.registerCorsConfiguration("/**", configuration);
+        // 3. 返回重新定义好的corsSource
+        return new CorsFilter(corsSource);
+    }
+}
