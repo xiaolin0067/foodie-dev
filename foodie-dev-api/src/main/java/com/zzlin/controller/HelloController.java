d372c0a05089b4e401a7bfe0b3ccbddb32338227
zlin <1562627895@qq.com>
2020-12-24 23:02:24 +0800

    Session测试

--- foodie-dev-api/src/main/java/com/zzlin/controller/HelloController.java
+++ foodie-dev-api/src/main/java/com/zzlin/controller/HelloController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 /**
  * @author zlin
  * @date 20201105
@@ -15,6 +18,20 @@ public class HelloController {
     @GetMapping("/hello")
     public Object hello() {
         return "Hello world";
+    }
+
+    private int uid = 0;
+    @GetMapping("/session")
+    public Object getSession(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        Object curUid = session.getAttribute("uid");
+        if (curUid == null) {
+            int setUid = ++uid;
+            session.setAttribute("uid", setUid);
+            session.setMaxInactiveInterval(60);
+            return setUid;
+        }
+        return curUid;
     }
 
 }
