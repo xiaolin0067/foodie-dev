90e2ab613a9c972ff4e44e6dcf1be1c3b874b6d3
zlin <1562627895@qq.com>
2021-01-24 14:36:53 +0800

    实现静态资源的映射

--- foodie-dev-api/src/main/java/com/zzlin/config/WebMvcConfig.java
+++ foodie-dev-api/src/main/java/com/zzlin/config/WebMvcConfig.java
@@ -4,13 +4,25 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @author zlin
  * @date 20210118
  */
 @Configuration
-public class WebMvcConfig {
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    /**
+     * 实现静态资源的映射
+     * @param registry registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("file:/workspaces/images/");
+    }
 
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
