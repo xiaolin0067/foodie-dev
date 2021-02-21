39cb381535ee720e2cca4287e8bad5bc9f52522b
zlin <1562627895@qq.com>
2021-02-21 22:11:36 +0800

    springboot打war包

---
+++ foodie-dev-api/src/main/java/com/zzlin/WarApp.java
@@ -0,0 +1,27 @@
+package com.zzlin;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * 打war包 step 4 增加war的启动类
+ * @author zlin
+ * @date 20210221
+ */
+public class WarApp extends SpringBootServletInitializer {
+    /**
+     * Configure the application. Normally all you would need to do is to add sources
+     * (e.g. config classes) because other settings have sensible defaults. You might
+     * choose (for instance) to add default command line arguments, or set an active
+     * Spring profile.
+     *
+     * @param builder a builder for the application context
+     * @return the application builder
+     * @see SpringApplicationBuilder
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 指向App启动类
+        return builder.sources(App.class);
+    }
+}
