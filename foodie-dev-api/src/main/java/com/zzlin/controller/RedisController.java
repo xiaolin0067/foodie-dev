c5b5cb81a7d516cb15d7880f4609dd1aa7d361dc
zlin <1562627895@qq.com>
2021-06-05 16:19:03 +0800

    整合redisOperator工具类

--- foodie-dev-api/src/main/java/com/zzlin/controller/RedisController.java
+++ foodie-dev-api/src/main/java/com/zzlin/controller/RedisController.java
@@ -1,5 +1,6 @@
 package com.zzlin.controller;
 
+import com.zzlin.utils.RedisOperator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,23 +22,27 @@ public class RedisController {
     private final static Logger LOGGER = LoggerFactory.getLogger(RedisController.class);
 
     @Autowired
-    private RedisTemplate redisTemplate;
+    private RedisOperator redisOperator;
 
     @GetMapping("/set")
     public Object set(String key, String value) {
         // opsForValue() 处理字符串的value
-        redisTemplate.opsForValue().set(key, value);
+//        redisTemplate.opsForValue().set(key, value);
+
+        redisOperator.set(key, value);
         return "OK";
     }
 
     @GetMapping("/get")
     public String get(String key) {
-        return (String) redisTemplate.opsForValue().get(key);
+//        return (String) redisTemplate.opsForValue().get(key);
+        return redisOperator.get(key);
     }
 
     @GetMapping("/del")
     public Object del(String key) {
-        redisTemplate.delete(key);
+//        redisTemplate.delete(key);
+        redisOperator.del(key);
         return "OK";
     }
 
