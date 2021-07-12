5d9f1468a128e0f2840e24487d5384d9c57b3a08
zlin <1562627895@qq.com>
2021-07-12 23:09:51 +0800

    更新mapping、删除索引、一般来说不建议通过客户端来对索引进行管理

--- foodie-dev-search/src/main/java/com/zzlin/es/pojo/Stu.java
+++ foodie-dev-search/src/main/java/com/zzlin/es/pojo/Stu.java
@@ -3,12 +3,14 @@ package com.zzlin.es.pojo;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 
 /**
  * @author zlin
  * @date 20210712
+ * 注意：@Document的shards与replicas属性设置无效
  */
-@Document(indexName = "stu", type = "_doc")
+@Document(indexName = "stu", type = "_doc", shards = 3, replicas = 0)
 public class Stu {
 
     /**
@@ -22,6 +24,15 @@ public class Stu {
 
     @Field(store = true)
     private Integer age;
+
+    @Field(store = true)
+    private Float money;
+
+    @Field(store = true, type = FieldType.Keyword)
+    private String sign;
+
+    @Field(store = true)
+    private String description;
 
     public Stu() {
     }
@@ -50,6 +61,30 @@ public class Stu {
 
     public Integer getAge() {
         return age;
+    }
+
+    public Float getMoney() {
+        return money;
+    }
+
+    public void setMoney(Float money) {
+        this.money = money;
+    }
+
+    public String getSign() {
+        return sign;
+    }
+
+    public void setSign(String sign) {
+        this.sign = sign;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public void setAge(Integer age) {
--- foodie-dev-search/src/test/java/com/test/es/ESTest.java
+++ foodie-dev-search/src/test/java/com/test/es/ESTest.java
@@ -22,12 +22,28 @@ public class ESTest {
     @Autowired
     private ElasticsearchTemplate esTemplate;
 
+    /**
+     * 一般来说不建议通过Java ElasticsearchTemplate client客户端来对索引进行管理（创建索引，更新映射，删除索引）
+     * 相当于数据库一样，程序只进行对数据的处理操作，不进行表的创建与修改删除
+     * 同时客户端对ES的索引进行管理也存在问题：
+     * 1、@Document的shards与replicas属性设置无效，无法设置分片与副本
+     * 2、@Field的FieldType类型不灵活
+     */
     @Test
     public void createIndexStu(){
         // 插入document数据，若索引不存在则进行创建，若存在则只进行新增
         Stu stu = new Stu(1002L, "hello", 19);
+        // 扩展字段的mapping的更新
+        stu.setMoney(66.6f);
+        stu.setSign("sign-1002");
+        stu.setDescription("a man can see the blood directly who was a real hero");
         IndexQuery indexQuery = new IndexQueryBuilder().withObject(stu).build();
         esTemplate.index(indexQuery);
+    }
+
+    @Test
+    public void deleteIndexStu(){
+        esTemplate.deleteIndex(Stu.class);
     }
 
 }
