62cc45722d3c5d57c4e415329062fcf3d8ac10c9
zlin <1562627895@qq.com>
2023-03-19 19:32:47 +0800

    jsonutil

--- foodie-dev-common/src/main/java/com/zzlin/utils/JsonUtils.java
+++ foodie-dev-common/src/main/java/com/zzlin/utils/JsonUtils.java
@@ -3,62 +3,71 @@ package com.zzlin.utils;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 
+/**
+ * spring自带json序列化工具
+ *
+ * @author zlin
+ * @date 20230319
+ */
+@Slf4j
 public class JsonUtils {
 
-    // 定义jackson对象
+    /**
+     * 定义jackson对象
+     */
     private static final ObjectMapper MAPPER = new ObjectMapper();
 
     /**
-     * 将对象转换成json字符串。
-     * @param data
-     * @return
+     * 将对象转换成json字符串
+     *
+     * @param data 要转化的对象
+     * @return 对象json字符串
      */
     public static String objectToJson(Object data) {
-    	try {
-			String string = MAPPER.writeValueAsString(data);
-			return string;
-		} catch (JsonProcessingException e) {
-			e.printStackTrace();
-		}
-    	return null;
-    }
-    
-    /**
-     * 将json结果集转化为对象
-     * 
-     * @param jsonData json数据
-     * @param beanType 对象中的object类型
-     * @return
-     */
-    public static <T> T jsonToPojo(String jsonData, Class<T> beanType) {
         try {
-            T t = MAPPER.readValue(jsonData, beanType);
-            return t;
-        } catch (Exception e) {
-        	e.printStackTrace();
+            return MAPPER.writeValueAsString(data);
+        } catch (JsonProcessingException e) {
+            log.error("objectToJson error", e);
         }
         return null;
     }
-    
+
+    /**
+     * 将json结果集转化为对象
+     *
+     * @param jsonData json数据
+     * @param beanType 对象中的object类型
+     * @return 转换后的对象
+     */
+    public static <T> T jsonToPojo(String jsonData, Class<T> beanType) {
+        try {
+            return MAPPER.readValue(jsonData, beanType);
+        } catch (Exception e) {
+            log.error("jsonToPojo error", e);
+        }
+        return null;
+    }
+
     /**
      * 将json数据转换成pojo对象list
-     * @param jsonData
-     * @param beanType
-     * @return
+     *
+     * @param jsonData 要转化为对象列表的json
+     * @param beanType 要转化为对象的类型
+     * @return 对象列表
      */
-    public static <T>List<T> jsonToList(String jsonData, Class<T> beanType) {
-    	JavaType javaType = MAPPER.getTypeFactory().constructParametricType(List.class, beanType);
-    	try {
-    		List<T> list = MAPPER.readValue(jsonData, javaType);
-    		return list;
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-    	
-    	return null;
+    public static <T> List<T> jsonToList(String jsonData, Class<T> beanType) {
+        JavaType javaType = MAPPER.getTypeFactory().constructParametricType(List.class, beanType);
+        try {
+            return MAPPER.readValue(jsonData, javaType);
+        } catch (Exception e) {
+            log.error("jsonToList error", e);
+        }
+
+        return null;
     }
-    
+
 }
