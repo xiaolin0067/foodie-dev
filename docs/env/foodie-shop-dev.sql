9eb71c26fa861cd22a2d07da71a92879cbf6da0b
lin <1562627895@qq.com>
2024-04-11 00:09:02 +0800

    create database

--- docs/env/foodie-shop-dev.sql
+++ docs/env/foodie-shop-dev.sql
@@ -19,6 +19,9 @@
 -- Table structure for table `carousel`
 --
 
+create database `foodie-shop-dev`;
+use `foodie-shop-dev`;
+
 DROP TABLE IF EXISTS `carousel`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
