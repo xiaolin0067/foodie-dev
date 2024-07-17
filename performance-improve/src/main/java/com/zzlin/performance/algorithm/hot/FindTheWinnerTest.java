85debfd8311d58f0b864153200688de31f591a75
lin <1562627895@qq.com>
2024-07-17 23:43:11 +0800

    单例模式5种可靠写法、约瑟夫环问题

---
+++ performance-improve/src/main/java/com/zzlin/performance/algorithm/hot/FindTheWinnerTest.java
@@ -0,0 +1,36 @@
+package com.zzlin.performance.algorithm.hot;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * <a href="https://leetcode.cn/problems/find-the-winner-of-the-circular-game/">约瑟夫问题</a>
+ * 最后一人的编号 约瑟夫问题
+ *
+ * @author pang
+ * @date 2024/7/17
+ */
+public class FindTheWinnerTest {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.findTheWinner(5, 2) == 3);
+        System.out.println(solution.findTheWinner(6, 5) == 1);
+    }
+}
+
+class Solution {
+    public int findTheWinner(int n, int k) {
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            queue.add(i);
+        }
+        int j = k - 1;
+        while (queue.size() > 1) {
+            for (int i = 0; i < j; i++) {
+                queue.add(queue.poll());
+            }
+            queue.poll();
+        }
+        return queue.peek();
+    }
+}
---
+++ performance-improve/src/main/java/com/zzlin/performance/algorithm/hot/SingletonPatternTest.java
@@ -0,0 +1,82 @@
+package com.zzlin.performance.algorithm.hot;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * 单例模式的5种写法
+ *
+ * @author pang
+ * @date 2024/7/17
+ */
+public class SingletonPatternTest {
+}
+
+class SingletonPattern1 {
+    private static final SingletonPattern1 INSTANCE = new SingletonPattern1();
+    private SingletonPattern1() {
+    }
+    public static SingletonPattern1 getInstance() {
+        return INSTANCE;
+    }
+    public void test() {
+    }
+}
+
+class SingletonPattern2 {
+    private volatile static SingletonPattern2 INSTANCE;
+    private SingletonPattern2() {
+    }
+
+    public static SingletonPattern2 getInstance() {
+        if (INSTANCE == null) {
+            synchronized (SingletonPattern2.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = new SingletonPattern2();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+    public void test() {
+    }
+}
+
+enum SingletonPattern3 {
+    INSTANCE;
+
+    public void test() {
+    }
+}
+
+class SingletonPattern4 {
+    private static class InstanceHolder {
+        private static final SingletonPattern4 INSTANCE = new SingletonPattern4();
+    }
+    private SingletonPattern4() {
+    }
+    public static SingletonPattern4 getInstance() {
+        return InstanceHolder.INSTANCE;
+    }
+    public void test() {
+    }
+}
+
+class SingletonPattern5 {
+    private static final AtomicReference<SingletonPattern5> INSTANCE = new AtomicReference<>();
+    private SingletonPattern5() {
+    }
+
+    public static SingletonPattern5 getInstance() {
+        if (INSTANCE.get() != null) {
+            return INSTANCE.get();
+        }
+        for (;;) {
+            if (INSTANCE.get() != null) {
+                return INSTANCE.get();
+            }
+            INSTANCE.compareAndSet(null, new SingletonPattern5());
+        }
+    }
+
+}
+
